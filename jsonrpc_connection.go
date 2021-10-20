e55358d0608a951a30f211a5b5e0c1b64b82789e
Cristian Maglie <c.maglie@bug.st>
2021-10-20 11:12:25 +0200

    Added support for request response

--- jsonrpc_connection.go
+++ jsonrpc_connection.go
@@ -8,80 +8,96 @@ import (
 	"net/http"
 	"net/textproto"
 	"strconv"
+	"sync"
 
 	"go.bug.st/json"
 )
 
 // Connection is a JSON RPC connection for LSP protocol
 type Connection struct {
-	in      *bufio.Reader
-	out     io.Writer
-	errors  []error
-	handler RequestHandler
+	in                  *bufio.Reader
+	out                 io.Writer
+	outMutex            sync.Mutex
+	errorHandler        func(error)
+	requestHandler      RequestHandler
+	notificationHandler NotificationHandler
 }
 
 // RequestHandler handles requests from a jsonrpc Connection.
-// If id is nil the message is a 'notification' otherwise is a 'request'.
-type RequestHandler func(ctx context.Context, id *IntOrString, method string, params *ArrayOrObject) error
+type RequestHandler func(ctx context.Context, method string, params *ArrayOrObject, respCallback func(result Any, err error))
+
+// NotificationHandler handles notifications from a jsonrpc Connection.
+type NotificationHandler func(ctx context.Context, method string, params *ArrayOrObject)
 
 // NewConnection starts a new
-func NewConnection(in io.Reader, out io.Writer, handler RequestHandler) *Connection {
+func NewConnection(in io.Reader, out io.Writer, requestHandler RequestHandler, notificationHandler NotificationHandler, errorHandler func(error)) *Connection {
 	conn := &Connection{
-		in:      bufio.NewReader(in),
-		out:     out,
-		handler: handler,
+		in:                  bufio.NewReader(in),
+		out:                 out,
+		requestHandler:      requestHandler,
+		notificationHandler: notificationHandler,
+		errorHandler:        errorHandler,
 	}
-	go conn.inLoop()
 	return conn
 }
 
-func (c *Connection) inLoop() {
+func (c *Connection) Run() {
 	in := textproto.NewReader(c.in)
 	for {
 		head, err := in.ReadMIMEHeader()
 		if err != nil {
-			c.errors = append(c.errors, err)
+			c.errorHandler(err)
 			c.Close()
 			return
 		}
 
 		httpHeader := http.Header(head)
 		l := httpHeader.Get("Content-Length")
-		httpHeader.Del("Content-Length")
 		dataLen, err := strconv.Atoi(l)
 		if err != nil {
-			c.errors = append(c.errors, err)
+			c.errorHandler(err)
 			c.Close()
 			return
 		}
 
 		jsonData := make([]byte, dataLen)
-		if _, err := io.ReadFull(in.R, jsonData); err != nil {
-			c.errors = append(c.errors, err)
+		if n, err := io.ReadFull(in.R, jsonData); err != nil {
+			c.errorHandler(err)
 			c.Close()
 			return
+		} else if n != dataLen {
+			c.errorHandler(fmt.Errorf("expected %d bytes but %d have been read", dataLen, n))
 		}
-
-		go c.handleRequest(jsonData)
+		c.handleRequest(jsonData)
 	}
 }
 
 func (c *Connection) handleRequest(jsonData []byte) {
 	var req RequestMessage
 	if err := json.Unmarshal(jsonData, &req); err == nil {
-		fmt.Printf("REQUEST: %+v\n", req)
-		c.handler(context.Background(), &req.ID, req.Method, req.Params)
+		c.requestHandler(context.Background(), req.Method, req.Params, func(result Any, resultErr error) {
+			resp := &ResponseMessage{
+				Message: Message{JSONRPC: "2.0"},
+				ID:      req.ID,
+				Result:  result,
+			}
+			_ = resultErr // TODO...
+			if sendErr := c.Send(resp); sendErr != nil {
+				c.errorHandler(fmt.Errorf("error sending response: %s", sendErr))
+				c.Close()
+			}
+		})
+
 		return
 	}
 
 	var notif NotificationMessage
 	if err := json.Unmarshal(jsonData, &notif); err == nil {
-		fmt.Printf("NOTIFICATION: %+v\n", notif)
-		c.handler(context.Background(), nil, notif.Method, notif.Params)
+		c.notificationHandler(context.Background(), notif.Method, notif.Params)
 		return
 	}
 
-	c.errors = append(c.errors, fmt.Errorf("invalid request: %s", string(jsonData)))
+	c.errorHandler(fmt.Errorf("invalid request: %s", string(jsonData)))
 	c.Close()
 }
 
@@ -89,6 +105,23 @@ func (c *Connection) Close() {
 
 }
 
-func (c *Connection) Send() {
+func (c *Connection) Send(data interface{}) error {
+	buff, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
 
+	c.outMutex.Lock()
+	defer c.outMutex.Unlock()
+	if _, err := fmt.Fprintf(c.out, "Content-Length: %d\r\n\r\n", len(buff)); err != nil {
+		return err
+	}
+	for len(buff) > 0 {
+		n, err := c.out.Write(buff)
+		if err != nil {
+			return err
+		}
+		buff = buff[n:]
+	}
+	return nil
 }
--- jsonrpc_test.go
+++ jsonrpc_test.go
@@ -2,12 +2,12 @@ package lsp
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 	"go.bug.st/json"
@@ -22,6 +22,14 @@ func TestRPCConnection(t *testing.T) {
 	"params": {
 	}
 }`
+	reqData2 := `
+{
+	"jsonrpc": "2.0",
+	"id": 2,
+	"method": "textDocument/didClose",
+	"params": {
+	}
+}`
 	notifData := `
 {
 	"jsonrpc": "2.0",
@@ -30,19 +38,37 @@ func TestRPCConnection(t *testing.T) {
 }`
 	testdata := fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(notifData), notifData)
 	testdata += fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(reqData), reqData)
+	testdata += fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(reqData2), reqData2)
+	fmt.Println(testdata)
 	resp := ""
-	conn := &Connection{
-		in: bufio.NewReader(strings.NewReader(testdata)),
-		handler: func(ctx context.Context, id *IntOrString, method string, params *ArrayOrObject) error {
-			resp += fmt.Sprintf("method=%v ID=%v params=%v\n", method, id, params.Value())
-			return nil
+	output := &bytes.Buffer{}
+	conn := NewConnection(
+		bufio.NewReader(strings.NewReader(testdata)),
+		output,
+		func(ctx context.Context, method string, params *ArrayOrObject, respCallback func(Any, error)) {
+			resp += fmt.Sprintf("REQ method=%v params=%v\n", method, params.Value())
+			respCallback(nil, nil)
 		},
-	}
-	conn.inLoop()
-	time.Sleep(100 * time.Millisecond)
-	require.Len(t, conn.errors, 1)
-	require.Equal(t, conn.errors[0], io.EOF)
-	require.Equal(t, "method=textDocument/didOpen ID=&[49] params=[123 10 9 125]\nmethod=initialized ID=<nil> params=[[49 50 51]]\n", resp)
+		func(ctx context.Context, method string, params *ArrayOrObject) {
+			resp += fmt.Sprintf("NOT method=%v params=%v\n", method, params.Value())
+		},
+		func(e error) {
+			if e == io.EOF {
+				return
+			}
+			resp += fmt.Sprintf("error=%s\n", e)
+		},
+	)
+	conn.Run() // Exits when input is fully consumed
+	conn.Close()
+	require.Equal(t,
+		"NOT method=initialized params=[[49 50 51]]\n"+
+			"REQ method=textDocument/didOpen params=[123 10 9 125]\n"+
+			"REQ method=textDocument/didClose params=[123 10 9 125]\n"+
+			"", resp)
+
+	//require.Equal(t, "", output.String())
+	fmt.Println(output.String())
 }
 
 func TestIntegerOrString(t *testing.T) {
