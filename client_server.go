4d0e0a7f60d472a4ef4d47460f5d91b02d567904
Cristian Maglie <c.maglie@bug.st>
2021-10-20 21:39:51 +0200

    Started Server implementation (draft)

--- client_server.go
+++ client_server.go
@@ -1,14 +1,47 @@
 package lsp
 
-import "context"
+import (
+	"context"
+	"io"
+
+	"go.bug.st/json"
+)
 
 // Client is an LSP client
 type Client struct{}
 
 // Server is an LSP Server
-type Server struct{}
+type Server struct {
+	handler ServerHandler
+	conn    *Connection
+}
 
 // ServerHandler is an LSP Server message handler
 type ServerHandler interface {
 	Initialize(ctx context.Context, conn Connection, params InitializeParams)
+}
+
+func NewServer(in io.Reader, out io.Writer, handler ServerHandler) *Server {
+	serv := &Server{}
+	serv.handler = handler
+	serv.conn = NewConnection(in, out, serv.RequestHandler, serv.NotificationHandler, serv.ErrorHandler)
+	return serv
+}
+
+func (serv *Server) ErrorHandler(e error) {
+}
+
+func (serv *Server) NotificationHandler(ctx context.Context, method string, params json.RawMessage) {
+}
+
+func (serv *Server) RequestHandler(ctx context.Context, method string, params json.RawMessage, respCallback func(json.RawMessage, error)) {
+	switch method {
+	case "initialize":
+	default:
+	}
+	respCallback(nil, nil)
+}
+
+func (serv *Server) Run() {
+	serv.conn.Run()
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.15
 replace go.bug.st/json => ../go-json
 
 require (
-	go.bug.st/json v1.0.0
 	github.com/arduino/go-paths-helper v1.4.0
+	github.com/davecgh/go-spew v1.1.0
 	github.com/stretchr/testify v1.6.1
+	go.bug.st/json v1.0.0
 )
