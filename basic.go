1c5c59653c2f0d62993a15d999a1ca2cb4f0d4f7
Cristian Maglie <c.maglie@bug.st>
2021-10-21 12:15:40 +0200

    Using lsp_basic.go for all basic structures

--- basic.go
+++
@@ -1,6 +0,0 @@
-package lsp
-
-type WorkDoneProgressParams struct {
-	// An optional token that a server can use to report work done progress.
-	WorkDoneToken *ProgressToken `json:"workDoneToken,omitempty"`
-}
--- lsp_basic.go
+++ lsp_basic.go
@@ -3,3 +3,8 @@ package lsp
 type WorkDoneProgressOptions struct {
 	WorkDoneProgress bool `json:"workDoneProgress,omitempty"`
 }
+
+type WorkDoneProgressParams struct {
+	// An optional token that a server can use to report work done progress.
+	WorkDoneToken *ProgressToken `json:"workDoneToken,omitempty"`
+}
